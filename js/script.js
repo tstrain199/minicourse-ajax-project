0dfecb0d3e0eb75320450c28be8cba6e2a2b06bc
tstrain199 <tstrain199@gmail.com>
2017-05-16 23:03:51 -0400

    Background image working

--- js/script.js
+++ js/script.js
@@ -14,7 +14,13 @@ function loadData() {
     // load streetview
 
     // YOUR CODE GOES HERE!
-
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var googleStr = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location=';
+    var apiKey = 'AIzaSyBLZUDNLNjSZ-i9qn22ZTfvp-KEmgMO2Ag';
+    var urlStr = googleStr.concat(streetStr,',',cityStr,'&key=',apiKey);
+    console.log(urlStr);
+    $( '.nytimes-container' ).append( '<img class="bgimg" src="'+urlStr+'">');
     return false;
 };
 
