e9dcf0d396f9427c01ced0d838d7b7079db16b27
Todd Strain <tstrain199@gmail.com>
2017-05-20 22:08:51 -0400

    Start of wikik

--- js/script.js
+++ js/script.js
@@ -21,19 +21,18 @@ function loadData() {
     // load streetview
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
-
+    // debugger;
 
     // load nytimes
 
     // YOUR CODE GOES HERE!
-    //var items = [];
     var nyt_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
     nyt_url += '?' + $.param({
       'api-key': "751ec68137734ff0a09da10dafe1be06",
       'q': address
     });
-    console.log(nyt_url);
 
+    // debugger;
     $.getJSON(nyt_url, function(data) {
         for (doc in data.response.docs) {
           //console.log(data.response.docs[doc].headline.main);
@@ -41,17 +40,29 @@ function loadData() {
           var lead = data.response.docs[doc].headline.main;
           var para = data.response.docs[doc].lead_paragraph;
           var item = "<li class='article'> <a href='" + href + "'>" + lead + "</a> <p>" + para + "</p>";
-
-          //console.log(item);
           $('#nytimes-articles').append(item);
         }
     })
-      .error(function() {
+      .fail(function() {
         var err_msg = "<h2 class='greeting'>New York Times Articles Could Not Be Found </h2>";
         $(err_msg).appendTo(".nytimes-container");
       });
 
+    // load wikipedia-links
+    var wiki_url = "https://en.wikipedia.org/w/api.php";
+    wiki_url += '?' + $.param({
+      'action': "opensearch",
+      'search': cityStr,
+      'format': "json"
+    });
+    console.log(wiki_url);
 
+    $.ajax({
+      url: wiki_url,
+      dataType: json,
+      success: wikiparse,
+    });
+    function wikiparse
 
         return false;
     };
